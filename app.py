6281a8aab81184703d0adb90b8280f9d52881fe3
Ag-idris772 <130905590+Ag-idris772@users.noreply.github.com>
2025-08-07 20:50:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown("""
         cursor: pointer;
     }
     .chat-bubble {
-        background-color: #0455A4;
+        background-color: #f4f6f9;
         color: white; /* <-- Added this line to fix mobile dark mode issue */
         padding: 1em;
         border-radius: 10px;
